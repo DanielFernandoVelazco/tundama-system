0322a98b9986c86e9b9582f1958ac7ef56e4981f
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-11-10 18:50:28 -0500

    Se crea module purchase

---
+++ backend/src/purchases/purchases.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PurchasesService } from './purchases.service';
+import { PurchasesController } from './purchases.controller';
+import { Purchase } from '../entities/purchase.entity';
+import { PurchaseItem } from '../entities/purchase-item.entity';
+import { Product } from '../entities/product.entity';
+import { Provider } from '../entities/provider.entity';
+
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([Purchase, PurchaseItem, Product, Provider]),
+    ],
+    controllers: [PurchasesController],
+    providers: [PurchasesService],
+    exports: [PurchasesService],
+})
+export class PurchasesModule { }
