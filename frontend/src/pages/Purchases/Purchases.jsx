914ad38ac1f2194edf30a22653bf47fd783eb028
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-11-13 19:37:13 -0500

    Modifica incluye los iconos compras

--- frontend/src/pages/Purchases/Purchases.jsx
+++ frontend/src/pages/Purchases/Purchases.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { purchaseService } from '../../services/purchaseService';
 import { providerService } from '../../services/providerService';
 import { productService } from '../../services/productService';
 import './Purchases.css';
+import LogoImage from '../../img/logo.png';
+import CustomerImage from '../../img/customer_icon.png';
 
 const Purchases = () => {
     const navigate = useNavigate();
@@ -213,11 +215,11 @@ const Purchases = () => {
         <div className="container sales-purchases-module-container">
             <div className="header">
                 <div className="header-left">
-                    <img src="/img/logo.png" alt="Logo" className="header-logo" />
+                    <img src={LogoImage} alt="Logo" className="header-logo" />
                     <h1>CARLOS MODULO COMPRAS</h1>
                 </div>
                 <div className="header-right">
-                    <img src="/img/purchases_icon.png" alt="Compras" className="header-icon" />
+                    <img src={CustomerImage} alt="Compras" className="header-icon" />
                     <span className="version">VERSION 1.0</span>
                     <button
                         className="button button-regresar"
